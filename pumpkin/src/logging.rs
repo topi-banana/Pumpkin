ef9800020b1d25543bfbbb7b1774cfaa1e9266ec
Sol ☆ <72232630+Solitarju@users.noreply.github.com>
2026-02-17 03:16:09 +0000

    fix: replace oldest log file when log filename id has reached maximum (#1565)
    * fix: replace oldest log file when log filename id has reached maximum
    
    * fix formatting
    
    * track oldest log file in single pass
    
    * warn when overwriting existing log
    
    * increase MAX_ATTEMPTS to 1000

--- pumpkin/src/logging.rs
+++ pumpkin/src/logging.rs
@@ -81,7 +81,7 @@ macro_rules! plugin_log {
 }
 
 const LOG_DIR: &str = "logs";
-const MAX_ATTEMPTS: u32 = 100;
+const MAX_ATTEMPTS: u32 = 1000;
 
 /// A wrapper for our logger to hold the terminal input while no input is expected in order to
 /// properly flush logs to the output while they happen instead of batched
@@ -157,12 +157,36 @@ impl GzipRollingLogger {
 
         let log_path = PathBuf::from(LOG_DIR);
 
+        let mut oldest_log = None;
+
         for id in 1..=MAX_ATTEMPTS {
             let filename = log_path.join(format!("{date_format}-{id}.log.gz"));
 
             if !filename.exists() {
                 return Ok(filename);
             }
+
+            let Ok(modified_time) = filename.metadata().and_then(|m| m.modified()) else {
+                continue;
+            };
+
+            if let Some((_, old_time)) = oldest_log {
+                if modified_time < old_time {
+                    oldest_log = Some((filename, modified_time));
+                }
+
+                continue;
+            }
+
+            oldest_log = Some((filename, modified_time));
+        }
+
+        if let Some((path, _)) = oldest_log {
+            eprintln!(
+                "Max log ids ({MAX_ATTEMPTS}) used for {date_format}; overwriting oldest log file: {}",
+                path.display()
+            );
+            return Ok(path);
         }
 
         Err(format!(
